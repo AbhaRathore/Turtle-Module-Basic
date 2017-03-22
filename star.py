81161785939b1e8c876d1e7278693337f9c4d750
AbhaRathore <rathore.abha30@gmail.com>
2017-03-22 18:45:50 +0530

    Star and polygon shapes

---
+++ star.py
@@ -0,0 +1,14 @@
+import turtle
+
+turtle.showturtle()
+turtle.right(72)
+turtle.forward(150)
+turtle.right(144)
+turtle.forward(150)
+turtle.right(144)
+turtle.forward(150)
+turtle.right(144)
+turtle.forward(150)
+turtle.right(144)
+turtle.forward(150)
+turtle.done()
