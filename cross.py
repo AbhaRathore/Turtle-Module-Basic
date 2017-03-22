343508f49b2c1899585f3928013c33a682da2029
AbhaRathore <rathore.abha30@gmail.com>
2017-03-22 10:55:13 +0530

    Some more shapes by using turtle.

---
+++ cross.py
@@ -0,0 +1,13 @@
+import turtle
+
+turtle.showturtle()
+turtle.penup()
+turtle.goto(-100, 0)
+turtle.pendown()
+turtle.forward(200)
+turtle.penup()
+turtle.goto(0, 100)
+turtle.pendown()
+turtle.right(90)
+turtle.forward(200)
+turtle.done()
